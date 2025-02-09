3adbeb799d1fa76a9047da4c514d788dcb4e5921
Mer Manahan <mermanahan@gmail.com>
2025-02-09 13:34:09 -0500

    updated bucket name

--- Data-Warehousing/load_yellow_taxi_data.py
+++ Data-Warehousing/load_yellow_taxi_data.py
@@ -6,7 +6,7 @@ import time
 
 
 #Change this to your bucket name
-BUCKET_NAME = "dezoomcamp_hw3_2025"  
+BUCKET_NAME = "dtc-de-course-448403-zoomcamp-bucket"  
 
 #If you authenticated through the GCP SDK you can comment out these two lines
 CREDENTIALS_FILE = "gcs.json"  
